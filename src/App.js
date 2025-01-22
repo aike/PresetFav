9610efbd06dbcdafde0ad6dbf31df00170eb8b3d
aike <aaiikkee@gmail.com>
2025-01-23 00:35:38 +0900

    add search

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { auth, signInWithGoogle, logout, setRating, subscribeUserRatings } from './firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 import { PRESETS } from './presets';
+import './App.css';
 
 function App() {
   const [user, setUser] = useState(null);
   // ユーザーが付けたレーティングを格納するオブジェクト。 { presetId: { rating: number, updatedAt: ... }, ...}
   const [userRatings, setUserRatings] = useState({});
+
+  const inputRef = React.useRef();
+  const [keyword, setKeyword] = useState("");
+  const [showList, setShowList] = useState(false);
+  const [filteredList, setFilteredList] = useState(PRESETS);
 
   useEffect(() => {
     // Auth状態を監視
@@ -24,8 +30,41 @@ function App() {
         setUserRatings({});
       }
     });
+
+    if (inputRef.current) {
+      const node = inputRef.current;
+      node.focus();
+      setShowList(true);
+    }
+
+    if (keyword === "") {
+      setFilteredList(PRESETS);
+      return;
+    }
+
+    const searchKeyword = keyword
+      .trim()
+      .toLowerCase();
+
+    if ((searchKeyword.length >= 1) && (searchKeyword.length <= 1)) {
+      setFilteredList([]);
+      return;
+    }
+    
+    //入力されたキーワードが空白のみの場合
+    if (searchKeyword === null) {
+      setFilteredList(PRESETS);
+      return;
+    }
+
+    const result = PRESETS.filter((preset) =>
+      (preset.name.toLowerCase().indexOf(searchKeyword) !== -1)
+    );
+    setFilteredList(result.length ? result : [["No Item Found"]]);
+
+    // TODO: この意味を調べる
     return () => unsubAuth();
-  }, []);
+  }, [keyword]);
 
   const handleRatingChange = async (presetId, newRating) => {
     if (!user) return;
@@ -33,8 +72,12 @@ function App() {
     await setRating(user.uid, presetId, newRating);
   };
 
+  const ListItems = (props) => (
+    <tr><td className="cat">{props.category}</td><td className="key">{props.keyassign}</td><td className="cmd">{props.command}</td></tr>
+  );
+
   return (
-    <div style={{ margin: '2rem' }}>
+    <div className='App'>
       <h1>Roland MC-101 Presets</h1>
 
       {/* ログイン/ログアウトUI */}
@@ -53,6 +96,28 @@ function App() {
       <hr />
 
       {/* プリセット一覧表示 */}
+      <div className="datatable-container">
+        <div className="header-tools">
+          <div className="search">
+            <input type="search" className="search-input" placeholder="Search..."
+              ref={inputRef}
+              onChange={(e) => setKeyword(e.target.value)}
+              onClick={() => setShowList(true)}
+            />
+          </div>
+          <div id="app_title">ALL YOUR CUBASE ARE BELONG TO US <span id="subtitle">- Cubase Keyassign Easy Search Tool -</span></div>
+        </div>        
+        <table className="datatable">
+          <thead>
+            <tr><th id="tab_cath">NUMBER</th><th id="tab_keyh">NAME</th><th id="tab_cmdh">CATEGORY</th></tr>
+          </thead>
+          <tbody>
+            {showList &&
+            filteredList.map((preset) => <ListItems key={preset.id} category={preset.number} keyassign={preset.name} command={preset.category} />)}
+          </tbody>
+        </table>
+      </div>
+      {/*
       <ul>
         {PRESETS.map((preset) => {
           const ratingData = userRatings[preset.id] || {};
@@ -62,12 +127,12 @@ function App() {
             <li key={preset.id} style={{ marginBottom: '1rem' }}>
               <div>
                 {preset.name} 
-                {/* タグの表示 */}
+                {// タグの表示 }
                 <span style={{ marginLeft: '1rem', color: 'black' }}>Bank {preset.bank}</span>
                 <span style={{ marginLeft: '1rem', color: 'gray' }}>[{preset.tags.join(', ')}]</span>
               </div>
               <div>
-                {/* ★レーティングUI(簡易版) */}
+                {// ★レーティングUI(簡易版) }
                 {[1,2,3,4,5].map(star => (
                   <span
                     key={star}
@@ -87,6 +152,7 @@ function App() {
           );
         })}
       </ul>
+      */}
     </div>
   );
 }
